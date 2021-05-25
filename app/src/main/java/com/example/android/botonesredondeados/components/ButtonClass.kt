17eea398d680a36f894670f6896a88b57e80279a
Andres Campuzano <acampuz1@gmail.com>
2021-05-25 14:48:16 -0500

    Project initialization

---
+++ app/src/main/java/com/example/android/botonesredondeados/components/ButtonClass.kt
@@ -0,0 +1,29 @@
+package com.example.android.botonesredondeados.components
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Typeface
+import android.util.AttributeSet
+import android.view.View
+
+class ButtonClass @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.FILL
+        textAlign = Paint.Align.CENTER
+        textSize = 55.0F
+        typeface = Typeface.create("", Typeface.BOLD)
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+    }
+
+}
